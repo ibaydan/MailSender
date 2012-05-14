9dd11341e3fc43484ad81ed17f7de29e722227f7
İsmail Baydan <ibaydan@gmail.com>
2012-05-14 12:38:05 +0300

    Start

---
+++ SmtpMailer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+
+namespace MailSender
+{
+    class SmtpMailer
+    {
+        private System.Net.Mail.MailMessage mailMessage;
+
+        public void Send()
+        {
+            System.Net.Mail.smtp
+            
+        }
+    }
+}
